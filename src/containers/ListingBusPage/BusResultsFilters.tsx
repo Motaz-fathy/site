ebdb95252e6501fdee792cadba1719cc58216af0
Motaz-fathy <wazaxxp@gmail.com>
2023-09-20 00:11:03 +0300

    add stepper in bus trip

--- src/containers/ListingBusPage/BusResultsFilters.tsx
+++ src/containers/ListingBusPage/BusResultsFilters.tsx
@@ -467,167 +467,164 @@ const BusResultsFilters: FC<BusResultsFiltersProps> = React.memo(props => {
             </form>
 
             <div className={`  ${Styled.scroll}`}>
-   <form className={`w-full flex justify-around items-center  `}>
+            <form className={`w-full flex justify-around items-center  `}>
 
-   <div></div>
-   <div className="w-[100px] h-60px"></div>
+                    
+            <div className={`${Styled.f4}`}>
+                <div className={`${Styled.f4_icon}`} onClick={handleF4}>
+                <svg className="mr-2" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+                    <path d="M10 1C12.755 1 15.455 1.232 18.083 1.678C18.616 1.768 19 2.234 19 2.774V3.818C19 4.11348 18.9418 4.40606 18.8287 4.67904C18.7157 4.95203 18.5499 5.20007 18.341 5.409L12.909 10.841C12.7001 11.0499 12.5343 11.298 12.4213 11.571C12.3082 11.8439 12.25 12.1365 12.25 12.432V15.359C12.2501 15.777 12.1337 16.1867 11.914 16.5423C11.6943 16.8978 11.3799 17.1851 11.006 17.372L7.75 19V12.432C7.75 12.1365 7.69181 11.8439 7.57874 11.571C7.46566 11.298 7.29993 11.0499 7.091 10.841L1.659 5.409C1.45007 5.20007 1.28434 4.95203 1.17126 4.67904C1.05819 4.40606 0.999997 4.11348 1 3.818V2.774C1 2.234 1.384 1.768 1.917 1.678C4.58757 1.22586 7.29143 0.999058 10 1Z" stroke="#1E1E1E" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>  Operator</div>
+                <div className={`${f4 ?  Styled.f4_container : Styled.f4_dis}`}>
+                {operators.length > 1 && <CheckBox
+                    handleChange={handleChange}
+                    name="operatorFilter"
+                    checkBoxArray={operators}
+                    title="Operators"
+                />}
+            <div>
 
-          
-<div className={`${Styled.f4}`}>
-    <div className={`${Styled.f4_icon}`} onClick={handleF4}>
-    <svg className="mr-2" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
-        <path d="M10 1C12.755 1 15.455 1.232 18.083 1.678C18.616 1.768 19 2.234 19 2.774V3.818C19 4.11348 18.9418 4.40606 18.8287 4.67904C18.7157 4.95203 18.5499 5.20007 18.341 5.409L12.909 10.841C12.7001 11.0499 12.5343 11.298 12.4213 11.571C12.3082 11.8439 12.25 12.1365 12.25 12.432V15.359C12.2501 15.777 12.1337 16.1867 11.914 16.5423C11.6943 16.8978 11.3799 17.1851 11.006 17.372L7.75 19V12.432C7.75 12.1365 7.69181 11.8439 7.57874 11.571C7.46566 11.298 7.29993 11.0499 7.091 10.841L1.659 5.409C1.45007 5.20007 1.28434 4.95203 1.17126 4.67904C1.05819 4.40606 0.999997 4.11348 1 3.818V2.774C1 2.234 1.384 1.768 1.917 1.678C4.58757 1.22586 7.29143 0.999058 10 1Z" stroke="#1E1E1E" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-        </svg>  Operator</div>
-    <div className={`${f4 ?  Styled.f4_container : Styled.f4_dis}`}>
-    {operators.length > 1 && <CheckBox
-        handleChange={handleChange}
-        name="operatorFilter"
-        checkBoxArray={operators}
-        title="Operators"
-    />}
-  <div>
-
-  </div>
-
-    </div>
-  </div>
-
-  
- <div className={`${Styled.f1}`}>
-    <div className={`${Styled.f1_icon}` }  onClick={handleF1}> time Bus </div>
-
-    <div className={`${ f1 ?  Styled.f1_container : Styled.f1_dis } mb-10 flex h-6 h-fit w-full flex-col rounded-lg bg-white p-3 shadow-md`}>
-        <h5 className="border-w border-b-2 p-2">Bus times</h5>
-        <div className="h-fit w-full ">
-            <h6 className="my-4 text-sm text-slate-500">{`Depart from (${from})`}</h6>
-            <div className="my-3 flex justify-between">
-                <span className="text-xs">{travel_atTimes[0]}</span>
-                <span className="text-xs">
-                    {
-                        travel_atTimes[
-                        travel_atTimes.length - 1
-                        ]
-                    }
-                </span>
             </div>
-            <Slider
-                range
-                pushable={true}
-                className="text-red-400"
-                min={0}
-                max={travel_atTimes.length}
-                defaultValue={[0, travel_atTimes.length]}
-                allowCross={false}
-                step={1}
-                onChange={(e: any) => setDepartureRange(e)}
-            />
-            <div className="h-fit w-full ">
-                <h6 className="my-4 text-sm text-slate-500">{`arrival to (${to})`}</h6>
-                <div className="my-3 flex justify-between">
-                    <span className="text-xs">
-                        {arrival_atTimes[0]}
-                    </span>
-                    <span className="text-xs">
-                        {
-                            arrival_atTimes[
-                            arrival_atTimes.length - 1
-                            ]
-                        }
-                    </span>
+
                 </div>
-                <Slider
-                    range
-                    pushable={true}
-                    className="text-red-400"
-                    min={0}
-                    max={arrival_atTimes.length}
-                    defaultValue={[0, arrival_atTimes.length]}
-                    allowCross={false}
-                    step={1}
-                    onChange={(e: any) => setArrivalRange(e)}
-                />
             </div>
-        </div>
-    </div>
 
- </div>
+            
+            <div className={`${Styled.f1}`}>
+                <div className={`${Styled.f1_icon}` }  onClick={handleF1}> time Bus </div>
 
-
-  <div className={`${Styled.f2}`}>
-   <div className={`${Styled.f2_icon}`} onClick={handleF2}>Price</div>
-    <div className={`${ f2 ? Styled.f2_container : Styled.f2_dis}`}>
-    {
-        nonDuplicatedPrices.length > 1 &&
-        <div className={`mb-10 flex h-6 h-fit w-full flex-col rounded-lg bg-white p-3 shadow-md`}>
-            <h5 className="border-w border-b-2 p-2">Price</h5>
-            <div className="h-fit w-full ">
-                <div className="my-3 flex justify-between">
-                    <span className="text-xs">{nonDuplicatedPrices[0]}</span>
-                    <span className="text-xs">
-                        {nonDuplicatedPrices[nonDuplicatedPrices.length - 1]}
-                    </span>
+                <div className={`${ f1 ?  Styled.f1_container : Styled.f1_dis } mb-10 flex h-6 h-fit w-full flex-col rounded-lg bg-white p-3 shadow-md`}>
+                    <h5 className="border-w border-b-2 p-2">Bus times</h5>
+                    <div className="h-fit w-full ">
+                        <h6 className="my-4 text-sm text-slate-500">{`Depart from (${from})`}</h6>
+                        <div className="my-3 flex justify-between">
+                            <span className="text-xs">{travel_atTimes[0]}</span>
+                            <span className="text-xs">
+                                {
+                                    travel_atTimes[
+                                    travel_atTimes.length - 1
+                                    ]
+                                }
+                            </span>
+                        </div>
+                        <Slider
+                            range
+                            pushable={true}
+                            className="text-red-400"
+                            min={0}
+                            max={travel_atTimes.length}
+                            defaultValue={[0, travel_atTimes.length]}
+                            allowCross={false}
+                            step={1}
+                            onChange={(e: any) => setDepartureRange(e)}
+                        />
+                        <div className="h-fit w-full ">
+                            <h6 className="my-4 text-sm text-slate-500">{`arrival to (${to})`}</h6>
+                            <div className="my-3 flex justify-between">
+                                <span className="text-xs">
+                                    {arrival_atTimes[0]}
+                                </span>
+                                <span className="text-xs">
+                                    {
+                                        arrival_atTimes[
+                                        arrival_atTimes.length - 1
+                                        ]
+                                    }
+                                </span>
+                            </div>
+                            <Slider
+                                range
+                                pushable={true}
+                                className="text-red-400"
+                                min={0}
+                                max={arrival_atTimes.length}
+                                defaultValue={[0, arrival_atTimes.length]}
+                                allowCross={false}
+                                step={1}
+                                onChange={(e: any) => setArrivalRange(e)}
+                            />
+                        </div>
+                    </div>
                 </div>
-                <Slider
-                    range
-                    pushable={true}
-                    className="text-red-400"
-                    min={0}
-                    max={nonDuplicatedPrices.length}
-                    defaultValue={[0, nonDuplicatedPrices.length - 1]}
-                    allowCross={false}
-                    step={1}
-                    onChange={e => setPriceRange(e)}
-                />
+
             </div>
-        </div>
-    }
-    </div>
-  </div>
-
-   <div className={`${Styled.f3}`}>
-    <div className={`${Styled.f3_icon}`} onClick={handleF3}>Seat classes</div>
-    <div className={`${f3 ? Styled.f3_container : Styled.f3_dis}`}>
-
-    {classes.length > 1 && <CheckBox
-        handleChange={handleChange}
-        name="seatsFilter"
-        checkBoxArray={classes}
-        title="Classes"
-    />}
-
-    </div>
-   </div>
-
-   <div className={`${Styled.f5}`}>
-    <div className={`${Styled.f5_icon}`} onClick={handleF5}>Departure station</div>
-    <div className={`${f5 ? Styled.f5_container : Styled.f5_dis}`}>
-
-    {departureStations.length > 1 && <CheckBox
-        handleChange={handleChange}
-        name="departureFilter"
-        checkBoxArray={departureStations}
-        title="departure Stations"
-
-    />}
-    </div>
-   </div>
-
-   <div className={`${Styled.f6}`}>
-    <div className={`${Styled.f6_icon}`} onClick={handleF6}>arrival station</div>
-    <div className={`${f6 ? Styled.f6_container : Styled.f6_dis}`}>
-
-    {arrivalStations.length > 1 && <CheckBox
-        handleChange={handleChange}
-        name="arrivalFilter"
-        checkBoxArray={arrivalStations}
-        title="Arrival Stations"
-    />}
-    </div>
-   </div>
 
 
+            <div className={`${Styled.f2}`}>
+            <div className={`${Styled.f2_icon}`} onClick={handleF2}>Price</div>
+                <div className={`${ f2 ? Styled.f2_container : Styled.f2_dis}`}>
+                {
+                    nonDuplicatedPrices.length > 1 &&
+                    <div className={`mb-10 flex h-6 h-fit w-full flex-col rounded-lg bg-white p-3 shadow-md`}>
+                        <h5 className="border-w border-b-2 p-2">Price</h5>
+                        <div className="h-fit w-full ">
+                            <div className="my-3 flex justify-between">
+                                <span className="text-xs">{nonDuplicatedPrices[0]}</span>
+                                <span className="text-xs">
+                                    {nonDuplicatedPrices[nonDuplicatedPrices.length - 1]}
+                                </span>
+                            </div>
+                            <Slider
+                                range
+                                pushable={true}
+                                className="text-red-400"
+                                min={0}
+                                max={nonDuplicatedPrices.length}
+                                defaultValue={[0, nonDuplicatedPrices.length - 1]}
+                                allowCross={false}
+                                step={1}
+                                onChange={e => setPriceRange(e)}
+                            />
+                        </div>
+                    </div>
+                }
+                </div>
+            </div>
 
-   </form>
+            <div className={`${Styled.f3}`}>
+                <div className={`${Styled.f3_icon}`} onClick={handleF3}>Seat classes</div>
+                <div className={`${f3 ? Styled.f3_container : Styled.f3_dis}`}>
+
+                {classes.length > 1 && <CheckBox
+                    handleChange={handleChange}
+                    name="seatsFilter"
+                    checkBoxArray={classes}
+                    title="Classes"
+                />}
+
+                </div>
+            </div>
+
+            <div className={`${Styled.f5}`}>
+                <div className={`${Styled.f5_icon}`} onClick={handleF5}>Departure station</div>
+                <div className={`${f5 ? Styled.f5_container : Styled.f5_dis}`}>
+
+                {departureStations.length > 1 && <CheckBox
+                    handleChange={handleChange}
+                    name="departureFilter"
+                    checkBoxArray={departureStations}
+                    title="departure Stations"
+
+                />}
+                </div>
+            </div>
+
+            <div className={`${Styled.f6}`}>
+                <div className={`${Styled.f6_icon}`} onClick={handleF6}>arrival station</div>
+                <div className={`${f6 ? Styled.f6_container : Styled.f6_dis}`}>
+
+                {arrivalStations.length > 1 && <CheckBox
+                    handleChange={handleChange}
+                    name="arrivalFilter"
+                    checkBoxArray={arrivalStations}
+                    title="Arrival Stations"
+                />}
+                </div>
+            </div>
+
+
+
+            </form>
    </div>
         </>
     );
